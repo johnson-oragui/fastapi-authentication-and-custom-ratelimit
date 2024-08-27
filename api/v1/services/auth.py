827b438880d20bb5642e0a23b997f17fea28a5ef
johnson-oragui <johnson.oragui@gmail.com>
2024-08-27 18:21:14 +0100

    feat: added service class for authentication

---
+++ api/v1/services/auth.py
@@ -0,0 +1,54 @@
+from typing import Annotated, Optional
+from fastapi import APIRouter, Depends, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2
+
+from api.v1.models import User
+from api.core.base.async_services import AsyncServices
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
+
+class AuthService(AsyncServices):
+    """
+    Service class for authentication
+    """
+    async def create(self):
+        """
+        Create
+        """
+        pass
+
+    async def fetch(self):
+        """
+        Create
+        """
+        pass
+
+    async def fetch_all(self):
+        """
+        Create
+        """
+        pass
+
+    async def update(self):
+        """
+        Create
+        """
+        pass
+
+    async def delete(self):
+        """
+        Create
+        """
+        pass
+
+    async def get_current_current_user(
+            token: Annotated[OAuth2, Depends(oauth2_scheme)]
+    ) -> Optional[User]:
+        """
+        
+        """
+        return None
+
+
+auth_service = AuthService()
