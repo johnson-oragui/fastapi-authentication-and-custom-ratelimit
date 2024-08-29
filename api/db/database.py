50f8d588de37c065ebcf9be2913c1145b451d263
johnson-oragui <johnson-oragui@gmail.com>
2024-08-29 20:21:03 +0100

    chore: updated database connection from sync to async

--- api/db/database.py
+++ api/db/database.py
@@ -1,30 +1,59 @@
+import asyncio
 from typing import AsyncIterator
 from contextlib import asynccontextmanager
 from sqlalchemy.orm import (
     DeclarativeBase,
-    sessionmaker
 )
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, pool
 from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import (
+    async_sessionmaker,
+    create_async_engine,
+    AsyncSession,
+    async_scoped_session
+)
 from decouple import config
 
+# Create an asynchronous engine with future usage enabled
 class Base(DeclarativeBase):
     metadata = MetaData(schema='public')
 
-DB_URL = config("DB_URL")
-engine = create_async_engine(url=DB_URL)
+DB_URL: str = config("DB_URL")
+engine = create_async_engine(
+    url=DB_URL,
+    future=True,
+    poolclass=pool.AsyncAdaptedQueuePool,
+    # The number of permanent connections to keep in the pool.
+    # This determines the base number of connections that will be created and maintained.
+    pool_size=5,
+    # The maximum number of connections that can be created 
+    # over and above the pool_size when all connections are in use.
+    max_overflow=10,
+    # The maximum time to wait for a connection from the pool if all 
+    # connections are busy before raising an error.
+    pool_timeout=30,
+    # Time in seconds to recycle (reconnect) a connection.
+    # This is useful to avoid issues with idle connections being closed by the database.
+    pool_recycle=18000
+)
 
-async_session_factory = sessionmaker(
+# Create an asynchronous session factory
+async_session_factory = async_sessionmaker(
     bind=engine,
     class_=AsyncSession,
     autoflush=False,
-    autocommit=False
+    expire_on_commit=False
+)
+
+
+AsyncSessionLocal = async_scoped_session(
+    session_factory=async_session_factory,
+    scopefunc=asyncio.current_task
 )
 
 async def create_tables():
     """
-    
+    Creates all tables
     """
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -32,13 +61,15 @@ async def create_tables():
 @asynccontextmanager
 async def get_db() -> AsyncIterator[AsyncSession]:
     """
-    
+    Dependency to provide a database session for each request.
+    Handles session lifecycle including commit and rollback.
     """
-    async with async_session_factory() as session:
+    async with AsyncSessionLocal() as session:
         try:
             yield session
+            await session.commit()
         except SQLAlchemyError:
-            session.rollback()
+            await session.rollback()
             raise
         finally:
-            session.close()
+            await session.close()
