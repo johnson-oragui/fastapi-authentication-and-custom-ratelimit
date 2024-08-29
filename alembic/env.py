1e1d9dbcd44c2c3679ba733e39fff9d339a8c224
johnson-oragui <johnson-oragui@gmail.com>
2024-08-29 20:20:22 +0100

    chore: updated alembic config to use async and sync for migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,20 @@
+# for running async functions
+import asyncio
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+# represents a connection to the database
+from sqlalchemy.engine import Connection
+from sqlalchemy.ext.asyncio import (
+    # represents an async engine for sqlalchemy
+    AsyncEngine,
+    # for creating async engine and async db operations.
+    create_async_engine
+)
+# provides access to alembic configuration and and context during migrations
 from alembic import context
-from decouple import config as decouple_config
+# BASE: The declarative base from SQLAlchemy where all 
+# models inherit from, used to collect metadata.
 from api.db.database import Base, DB_URL
 from api.v1.models import *
 
@@ -12,21 +22,25 @@ from api.v1.models import *
 # access to the values within the .ini file in use.
 config = context.config
 
-DATABASE_URL = DB_URL.replace('asyncpg', 'psycopg2')
-
+# sets the sqlalchemy.url option in the alembic configuration dynamically
+# to the value or DB_URL
 config.set_main_option(
     name='sqlalchemy.url',
-    value=DATABASE_URL
+    value=DB_URL
 )
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+    # Configures logging according to the Alembic configuration file if it exists.
+    # This is important for tracking and debugging migration processes.
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
+# Sets the target_metadata to the metadata of the SQLAlchemy base.
+# This allows Alembic to auto-detect schema changes by inspecting the models.
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -46,7 +60,10 @@ def run_migrations_offline() -> None:
     script output.
 
     """
+    # Retrieves the database URL from the Alembic configuration.
     url = config.get_main_option("sqlalchemy.url")
+    # but would use the value set dynamically
+
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -57,6 +74,32 @@ def run_migrations_offline() -> None:
     with context.begin_transaction():
         context.run_migrations()
 
+# support for sync
+def do_run_migrations(connection: Connection) -> None:
+    """Runs the migrations using a given database connection.
+    It's used for synchronous operations.
+
+    """
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+# support for async
+async def run_async_migrations(engine: AsyncEngine):
+    """Handles running migrations using an asynchronous engine.
+    """
+    # Asynchronously connects to the database.
+    async with engine.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+        # Runs the synchronous migration function using the async connection.
+
+    await engine.dispose()
+    # Closes the engine and releases all resources.
+    
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
@@ -65,22 +108,30 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    # Checks if a connection is already available in the Alembic context.
+    engine = config.attributes.get('connection', None)
 
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
+    # Creates an asynchronous engine if one isn't provided.
+    if not engine:
+        engine = create_async_engine(
+            url=DB_URL,
+            future=True,
+            poolclass=pool.NullPool
         )
 
-        with context.begin_transaction():
-            context.run_migrations()
+    # run the asynchronous migrations if the engine is asynchronous
+    if isinstance(engine, AsyncEngine):
+        asyncio.run(run_async_migrations(engine))
+    else:
+        # runs the migrations synchronously.
+        do_run_migrations(engine.connect())
 
 
+# entry point
+# Determines whether to run migrations in offline or online mode based on the Alembic context.
 if context.is_offline_mode():
+    # Checks if Alembic is running in offline mode.
     run_migrations_offline()
 else:
+    # or run_migrations_online() accordingly.
     run_migrations_online()
