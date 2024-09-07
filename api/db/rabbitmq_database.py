a36c85f4d08abc721dc0f389781895cf9b200591
johnson-oragui <johnson-oragui@gmail.com>
2024-09-07 20:23:40 +0100

    feat: resolved to synchronous pika

--- api/db/rabbitmq_database.py
+++ api/db/rabbitmq_database.py
@@ -1,81 +1,27 @@
-import asyncio
-from typing import Dict, Any
-from datetime import datetime, timezone, timedelta
-import aio_pika
-from typing import AsyncGenerator
-from contextlib import asynccontextmanager
-from decouple import config
+import pika
+from contextlib import contextmanager
+import pika.exceptions
 
-RABBITMQ_URL: str = config("RABBITMQ_URL")
+from api.utils.settings import settings
 
-@asynccontextmanager
-async def get_rabbitmq() -> AsyncGenerator[aio_pika.RobustConnection, None]:
-    connection = await aio_pika.connect_robust(RABBITMQ_URL)
-    try:
-        yield connection
-    finally:
-        await connection.close()
+RABBITMQ_URL: str = settings.RABBITMQ_URL
 
-RATE_LIMITS = {
-    # max 5 attempts, 5 minutes penalty initially
-    'login': {
-        'max_attempts': 5,
-        'penalty_base': 5
-    },
-    # 10 requests per minute
-    'register': {
-        'max_requests':10,
-        'interval': 60
-    },
-    # 50 requests per minute
-    'other_route': {
-        'max_requests':50,
-        'interval': 60
-    }
-}
 
-PENALTIES: Dict[str, datetime] = {}
-# Store user attempts and penalties
-USER_ATTEMPTS: Dict[str, Dict[str, Any]] = {}
-
-async def rate_limit_worker(
-    channel: aio_pika.RobustConnection,
-    method: aio_pika.abc.AbstractIncomingMessage,
-    properties: aio_pika.abc.AbstractRobustExchange,
-    body: bytes
-):
+@contextmanager
+def get_rabbitmq_sync():
     """sumary_line
     
     Keyword arguments:
     argument -- description
     Return: return_description
     """
-    data: str = body.decode()
-    user_ip, route = data.split(',')
-    now: datetime = datetime.now(timezone.utc)
-
-    if route in RATE_LIMITS:
-        rate_limits: dict = RATE_LIMITS[route]
-
-        if route == 'login':
-            attempts: dict = USER_ATTEMPTS.get(
-                user_ip,
-                {
-                    'count': 0,
-                    'last_attempt': now
-                }
-            )
-            attempts['count'] += 1
-            attempts['last_attempt'] = now
-            USER_ATTEMPTS[user_ip] = attempts
-
-            if attempts['count'] > rate_limits['max_attempts']:
-                penalty_end = PENALTIES.get(
-                    user_ip,
-                    now - timedelta(minutes=1)
-                )
-                if now < penalty_end:
-                    penalty_end = rate_limits['penalty_base']
+    params = pika.ConnectionParameters('localhost')
+    connection = pika.BlockingConnection(params)
+    try:
+        yield connection
+    except pika.exceptions.AMQPConnectionError as exc:
+        print(f'error setting up pika sync: {exc}')
+        raise
 
 
 # Run the async function
