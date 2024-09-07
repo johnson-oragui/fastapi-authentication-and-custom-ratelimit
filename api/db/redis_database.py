474bed4c80155716641c4a4b136c2620a4ccdea1
johnson-oragui <johnson-oragui@gmail.com>
2024-09-07 20:24:01 +0100

    feat: resolved to synchronous redis

--- api/db/redis_database.py
+++ api/db/redis_database.py
@@ -1,37 +1,20 @@
-import asyncio
-import aioredis
-from typing import AsyncGenerator
-from contextlib import asynccontextmanager
-from decouple import config
+import redis
+from contextlib import contextmanager
+import redis.client
 
-REDIS_URL: str = config("REDIS_URL")
+from api.utils.settings import settings
 
-@asynccontextmanager
-async def get_redis() -> AsyncGenerator[aioredis.Redis, None]:
-    """
-    Asynchronous context manager to provide a Redis connection.
-    Manages setup and teardown of the Redis connection.
-    """
-    redis = await aioredis.from_url(
-        url=REDIS_URL,
-        max_connections=10,
-        decode_responses=True
+
+REDIS_URL: str = settings.REDIS_URL
+
+
+@contextmanager
+def get_redis_sync():
+    conn = redis.from_url(
+        url=REDIS_URL
     )
-
     try:
-        yield redis
-    finally:
-        await redis.close()
-
-async def example_redis_operation():
-    """
-    Example function to demonstrate using Redis connection
-    within the context manager.
-    """
-    async with get_redis() as redis:
-        await redis.set("example_key", "example_value")
-        value = await redis.get("example_key")
-        print(f"Retrieved value from Redis: {value}")
-
-
-asyncio.run(example_redis_operation())
+        yield conn
+    except Exception as exc:
+        print(exc)
+        raise
