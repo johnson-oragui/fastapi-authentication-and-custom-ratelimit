b83b5d5708e42d19463c7cb7b53ab1a453d3db3e
johnson-oragui <johnson-oragui@gmail.com>
2024-09-10 20:37:57 +0100

    feat: added settings class for all env variables

--- api/utils/settings.py
+++ api/utils/settings.py
@@ -17,4 +17,13 @@ class Settings:
     REGISTER_MAX_ATTEMPTS: int = int(config('REGISTER_MAX_ATTEMPTS'))
     OTHERS_MAX_ATTEMPTS: int = int(config('OTHERS_MAX_ATTEMPTS'))
 
+    SECRET_KEY: str = str(config('SECRET_KEY'))
+    ALGORITHM: str = str(config('ALGORITHM'))
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = int(config('ACCESS_TOKEN_EXPIRE_MINUTES'))
+    REFRESH_TOKEN_EXPIRE: int = int(config('REFRESH_TOKEN_EXPIRE'))
+
+    INITIAL_LOCKOUT_DURATION: int = int(config('INITIAL_LOCKOUT_DURATION'))
+    MAX_PENALTY_DURATION: int = int(config('MAX_PENALTY_DURATION'))
+    LOCKOUT_THRESHOLD: int = int(config('LOCKOUT_THRESHOLD'))
+
 settings = Settings()
