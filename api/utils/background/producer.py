eb40fa29eac8283ba2f080960598d1e1f7e161c6
johnson-oragui <johnson-oragui@gmail.com>
2024-09-10 20:39:24 +0100

    feat: added producer for login-attempt-handler

--- api/utils/background/producer.py
+++ api/utils/background/producer.py
@@ -35,3 +35,38 @@ def send_to_queue_sync(message_body: str):
             )
         )
     print('message_body: ', message_body)
+
+
+def handle_login_attempt(user_id: str):
+    """Publishes a message to the login_attempt_exchange with a routing key.
+    
+    Args:
+        message_body: The message to publish
+    Return:
+        None
+
+    """
+    with get_rabbitmq_sync() as connection:
+        channel = connection.channel()
+
+        channel.exchange_declare(
+            exchange='login_attempt_exchange',
+            exchange_type='direct',
+            durable=True
+        )
+
+        channel.queue_declare(
+            queue='login_attempt_queue',
+            durable=True
+        )
+
+        channel.basic_publish(
+            exchange='login_attempt_exchange',
+            routing_key='login_attempt',
+            body=user_id.encode(),
+            properties=pika.BasicProperties(
+                delivery_mode=DeliveryMode.Persistent,
+                content_type='text/plain'
+            )
+        )
+        print('message_body: ', user_id)
