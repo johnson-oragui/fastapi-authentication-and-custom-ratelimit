550c7c9c918e155876d9feeead2659ce051b4294
johnson-oragui <johnson-oragui@gmail.com>
2024-09-10 20:31:15 +0100

    feat: added connection args to redis connection

--- api/db/redis_database.py
+++ api/db/redis_database.py
@@ -11,10 +11,14 @@ REDIS_URL: str = settings.REDIS_URL
 @contextmanager
 def get_redis_sync():
     conn = redis.from_url(
-        url=REDIS_URL
+        url=REDIS_URL,
+        connection_pool_kwargs={'minsize': 1, 'maxsize': 10},
+        retry_on_error=True,
+        retry_on_timeout=True,
+        decode_responses=True
     )
     try:
         yield conn
     except Exception as exc:
-        print(exc)
+        print(f"Redis connection error: {exc}")
         raise
