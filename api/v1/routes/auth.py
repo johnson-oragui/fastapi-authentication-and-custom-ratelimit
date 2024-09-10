aa7373f9926cd39eae20c0cac0bd896d04cc7b1d
johnson-oragui <johnson-oragui@gmail.com>
2024-09-10 20:41:52 +0100

    feat: added token route for openapi docs authentication, updated login and register routes with auth_service class instance

--- api/v1/routes/auth.py
+++ api/v1/routes/auth.py
@@ -1,15 +1,28 @@
-from typing import Annotated, Optional
+from typing import Annotated
 from fastapi import APIRouter, Depends, status, Request
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.utils.check_rate_limit import check_rate_limits_sync
 from api.utils.background.producer import send_to_queue_sync
+from api.v1.services.auth import (RegisterUserResponse,
+                                  auth_service,
+                                  RegisterUserSchema,
+                                  oauth2_scheme,
+                                  OAuth2,
+                                  AccessToken)
+from api.db.database import get_db
+from api.v1.schemas.user import LoginUserSchema, LoginUserResponse
 
 
 auth = APIRouter(prefix='/auth', tags=['AUTH'])
 
 @auth.post('/login',
-           status_code=status.HTTP_200_OK)
-async def login(request: Request, login_schema: dict):
+           status_code=status.HTTP_200_OK,
+           response_model=LoginUserResponse)
+async def login(request: Request,
+                login_schema: LoginUserSchema,
+                db: Annotated[AsyncSession, Depends(get_db)]):
     """sumary_line
     
         Keyword arguments:
@@ -22,40 +35,60 @@ async def login(request: Request, login_schema: dict):
     path: str = request.url.path
     message_body: str = f'{user_ip},{path}'
     send_to_queue_sync(message_body)
-    return {'message': 'Login attempt recorded'}
+    return await auth_service.login_user(
+        login_schema.username,
+        login_schema.password,
+        db
+    )
 
 
 @auth.post('/register',
-           status_code=status.HTTP_200_OK)
-async def register(request: Request, login_schema: dict):
-    """sumary_line
-    
-        Keyword arguments:
-            argument -- description
-        Return:
-            return_description
+           status_code=status.HTTP_200_OK,
+           response_model=RegisterUserResponse)
+async def register(request: Request,
+                   register_schema: RegisterUserSchema,
+                   db: Annotated[AsyncSession, Depends(get_db)]):
+    """Registers a user.
     """
     check_rate_limits_sync(request)
     user_ip: str = request.client.host
     path: str = request.url.path
     message_body: str = f'{user_ip},{path}'
     send_to_queue_sync(message_body)
-    return {'message': 'Register attempt recorded'}
+    return await auth_service.create(register_schema, db)
 
+@auth.post('/token',
+           status_code=status.HTTP_200_OK,
+           response_model=AccessToken,
+           include_in_schema=False)
+async def token(request: Request,
+                form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                db: Annotated[AsyncSession, Depends(get_db)]):
+    """Logs in a user.
+    """
+    check_rate_limits_sync(request)
+    user_ip: str = request.client.host
+    path: str = request.url.path
+    message_body: str = f'{user_ip},{path}'
+    send_to_queue_sync(message_body)
+    
+    return await auth_service.oauth2_authenticate(
+        form_data.username,
+        form_data.password,
+        db
+    )
 
 @auth.post('/others',
            status_code=status.HTTP_200_OK)
-async def get(request: Request, login_schema: dict):
-    """sumary_line
-    
-        Keyword arguments:
-            argument -- description
-        Return:
-            return_description
+async def get(request: Request,
+              token: Annotated[OAuth2, Depends(oauth2_scheme)],
+              db: Annotated[AsyncSession, Depends(get_db)]):
+    """Placeholder for other routes.
     """
     check_rate_limits_sync(request)
     user_ip: str = request.client.host
     path: str = request.url.path
     message_body: str = f'{user_ip},{path}'
     send_to_queue_sync(message_body)
-    return {'message': 'get attempt recorded'}
+    await auth_service.get_current_active_user(token, db)
+    return {'message': 'others route attempt recorded'}
